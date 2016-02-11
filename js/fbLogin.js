3ffb478a8a476e4292e0fe25123181433efc25b0
Miller Augusto <miller_cit@ciandt.com>
2016-02-11 11:01:23 -0200

    fb login

---
+++ js/fbLogin.js
@@ -0,0 +1,55 @@
+function statusChangeCallback(response) {
+    // console.log('statusChangeCallback');
+    // console.log(response);
+
+    if (response.status === 'connected') {
+        testAPI();
+    } else if (response.status === 'not_authorized') {
+        document.getElementById('status').innerHTML = 'Please log ' + 'into this app.';
+    } else {
+        document.getElementById('status').innerHTML = 'Please log ' + 'into Facebook.';
+    }
+}
+
+function checkLoginState() {
+    FB.getLoginStatus(function (response) {
+        statusChangeCallback(response);
+    });
+}
+
+window.fbAsyncInit = function () {
+    FB.init({
+        appId: '1521673121462430',
+        cookie: true,
+        xfbml: true,
+        version: 'v2.2'
+    });
+    FB.getLoginStatus(function (response) {
+        statusChangeCallback(response);
+    });
+};
+
+(function (d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s);
+    js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+function testAPI() {
+    FB.api('/me', function (response) {
+        console.log(response);
+        FB.api(
+            "/" + response.id + "/picture",
+            function (r) {
+                if (r && !r.error) {
+                    console.log(r);
+
+                    document.getElementById('status').innerHTML = '<img src="' + r.data.url + '"> ' + response.name + '!';
+                }
+            }
+        );
+    });
+}
